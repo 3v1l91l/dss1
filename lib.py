854bec20c8e8e6c0624a457bb45e4ee81815badb
Misha Zhukov <3v1l91l@gmail.com>
2018-02-01 12:53:26 +0200

    m

--- lib.py
+++ lib.py
@@ -4,7 +4,7 @@ import os
 def load_data(data_path='.'):
     costs_df = pd.read_csv(os.path.join(data_path, 'Costs.csv'), parse_dates=['date_created'], dayfirst=False)
     relations_df = pd.read_csv(os.path.join(data_path, 'Relations.csv'), usecols=['name', 'chanel', 'id_partner'])
-    users_df = pd.read_csv(os.path.join(data_path, 'Users.csv'), 
+    users_df = pd.read_csv(os.path.join(data_path, 'Users.csv'),
                            usecols=['id', 'Reg_date', 'name', 'id_partner'], parse_dates=['Reg_date'], dayfirst=False)
     visits_df = pd.read_csv(os.path.join(data_path, 'Visits.csv'), parse_dates=['Visit_date'], dayfirst=False)
     orders_df = pd.read_csv(os.path.join(data_path, 'Orders.csv'), parse_dates=['Order Date'], dayfirst=False)
